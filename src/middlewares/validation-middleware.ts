714233cd012023be05112a9a5720ad49a1b938a6
Boris <borya_moiseev_2002@mail.ru>
2024-04-05 01:43:48 +0900

    err

--- src/middlewares/validation-middleware.ts
+++ src/middlewares/validation-middleware.ts
@@ -4,7 +4,5 @@ export const validationMiddleware = [
     body('email', 'Некорректный почтовый адрес').trim().isEmail(),
     body('password', 'Пароль должен быть не меньше 4 и не больше 32 символов')
         .trim()
-        .matches(/^[a-zA-Z]+$/)
-        .withMessage('Поле должно содержать только латинские буквы')
         .isLength({ min: 4, max: 32 }),
 ];
